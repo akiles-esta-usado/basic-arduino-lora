02509da406152561088c6461e45edb20519ae7d9
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2022-11-13 20:49:22 -0300

    Updated fake charging station mesaurement

--- lib/cs/cs.cpp
+++ lib/cs/cs.cpp
@@ -2,6 +2,11 @@
 #include "cs.h"
 #include "capture.h"
 
+static const uint32_t min_voltage = 200;
+static const uint32_t max_voltage = 300;
+static const uint32_t increment = 10;
+
+static uint32_t actual_voltage = min_voltage;
 
 void CS_init()
 {
@@ -12,7 +17,14 @@ void CS_init()
 void CS_capture()
 {
     Serial.println("== CS_capture begin ==");
-    capture.CS_charge += 1;
+
+    if (max_voltage < actual_voltage)
+        actual_voltage = min_voltage;
+
+    capture.CS_charge = actual_voltage;
+
+    actual_voltage += increment;
+
     Serial.println("== CS_capture end ==");
     return;
 }
